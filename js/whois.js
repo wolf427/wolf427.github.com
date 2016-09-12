743f525be7ce705ea3c448775e88f70763bb37a5
wolf427 <wolf_fei@foxmail.com>
2016-09-12 20:59:21 +0800

    change jquery resource add new text.json resource for test

--- js/whois.js
+++ js/whois.js
@@ -11,6 +11,12 @@ $.get("resources/relations.json",function(data){
 	relations_network = data;
 },"json");
 
+ function getUrlParam(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); 
+	var r = window.location.search.substr(1).match(reg);  
+	if (r != null) return unescape(r[2]); return null; //返回参数值
+}
+
 var appellations;
 function initAppellations(address){
 	$.get("resources/dialect_appellation.json",function(data){
